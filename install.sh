d2185e4235a5711279fd2a1417a7829efa4e131b
Austin Walker <awalker4@umassd.edu>
2014-07-24 13:19:32 -0400

    reorganize

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 # install.sh
 # When on a fresh system, symlink all the relevant files back to here
 
-dir=~/.dotfiles
+confDir=~/.dotfiles/conf
+scriptDir=~/.dotfiles/scripts
 olddir=~/dotfiles_old
-rcfiles=$(ls | grep 'rc\|conf')
+
+rcfiles=$(ls conf)
 scripts=$(ls scripts)
 
 if [[ ! -d $olddir ]]; then
@@ -14,19 +16,23 @@ fi
 echo "** Linking dotfiles **"
 for file in $rcfiles; do
     if [[ -f ~/.$file ]]; then
-        mv ~/.$file $olddir
+        mv ~/.$file $olddir/$file
+    elif [[ -h ~/.$file ]]; then
+        rm ~/.$file
     fi
 
     echo "Linking $file to ~/.$file"
-    ln -s $dir/$file ~/.$file
+    ln -s $confDir/$file ~/.$file
 done
 
 echo "** Linking scripts **"
 for file in $scripts; do
     if [[ -f ~/bin/$file ]]; then
         mv ~/bin/$file $olddir
+    elif [[ -h ~/bin/$file ]]; then
+        rm ~/bin/$file
     fi
 
     echo "Linking $file to ~/bin/$file"
-    ln -s $dir/scripts/$file ~/bin/$file
+    ln -s $scriptDir/$file ~/bin/$file
 done
