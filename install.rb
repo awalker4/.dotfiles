a80e9dc709549bc8b2a36ffc9659dd7afe5d27cc
Austin Walker <awalker4@umassd.edu>
2014-10-27 22:07:35 -0400

    Fix up install script

--- install.rb
+++ install.rb
@@ -1,13 +1,18 @@
 #/usr/bin/env ruby
+
 # Inspired by http://errtheblog.com/posts/89-huba-huba
+
 # This is idempotent, meaning you can run it over and over again without fear of
 # breaking anything. Use it as an installer or to upgrade after merging from an
 # upstream fork.
+
 home = File.expand_path('~')
+
 Dir['*'].each do |file|
-next if file =~ /install/ || file =~ /README/
-target = File.join(home, ".#{file}")
-`ln -ns #{File.expand_path file} #{target}`
+    next if file =~ /install/ || file =~ /README/ || file =~ /aliases/
+    target = File.join(home, ".#{file}")
+    `ln -ns #{File.expand_path file} #{target}`
 end
+
 `git submodule sync`
 `git submodule update --init --recursive`
