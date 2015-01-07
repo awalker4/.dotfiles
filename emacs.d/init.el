a4c8d92e5bc3a145380b9237d6318408fdc2ab53
Austin Walker <awalker4@umassd.edu>
2015-01-07 18:30:34 -0500

    Don't use pp-eval-last-sexp

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -694,9 +694,6 @@ PACKAGE is installed and the current version is deleted."
           (lambda ()
             ;; Use spaces, not tabs.
             (setq indent-tabs-mode nil)
-            ;; Pretty-print eval'd expressions.
-            (define-key emacs-lisp-mode-map
-              "\C-x\C-e" 'pp-eval-last-sexp)
             (define-key emacs-lisp-mode-map
               "\r" 'reindent-then-newline-and-indent)))
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
