c5e13575c1e47902dbc5fe2f875f2d286619024a
Austin Walker <awalker4@umassd.edu>
2015-03-31 11:16:49 -0400

    Add binding for ff-find-other-file, fix org templates

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -76,6 +76,7 @@
     paredit               ; minor mode for editing parentheses
     powerline             ; Rewrite of Powerline
     projectile            ; Easy navigation for files in a project
+    skewer-mode           ; Use the browser as a Javascript repl
     slime                 ; Superior Lisp Interaction Mode for Emacs
     solarized-theme       ; Great color theme
     undo-tree             ; Treat undo history as a tree
@@ -358,12 +359,13 @@ PACKAGE is installed and the current version is deleted."
   "#" 'aw/split-window-right-and-switch
   "=" 'balance-windows)
 
-;; Buffer Stuff
+;; Buffer and file stuff
 (evil-leader/set-key
   "bb" 'helm-mini
   "bk" 'kill-buffer
   "bl" 'ibuffer
   "bm" 'bookmark-jump
+  "bo" 'ff-find-other-file
   "bs" 'save-buffer
   "bw" 'write-file)
 
@@ -965,87 +967,9 @@ PACKAGE is installed and the current version is deleted."
 ;;     make it a lot easier to compare things.
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#839496")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
- '(custom-safe-themes
-   (quote
-    ("7c89d1df5a1dd624983f6d107aced89a4b3d787b20997e5c6cff30cc1ba1b55d" default)))
+ '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(ediff-diff-options "-w")
- '(ediff-split-window-function (quote split-window-horizontally))
- '(ediff-window-setup-function (quote ediff-setup-windows-plain))
- '(fci-rule-color "#073642")
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#002b36" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#93a1a1")
- '(highlight-tail-colors
-   (quote
-    (("#073642" . 0)
-     ("#546E00" . 20)
-     ("#00736F" . 30)
-     ("#00629D" . 50)
-     ("#7B6000" . 60)
-     ("#8B2C02" . 70)
-     ("#93115C" . 85)
-     ("#073642" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
- '(hl-fg-colors
-   (quote
-    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
- '(magit-diff-use-overlays nil)
- '(org-emphasis-alist
-   (quote
-    (("*" bold)
-     ("/" italic)
-     ("_" underline)
-     ("=" org-verbatim verbatim)
-     ("~" org-code verbatim)
-     ("+"
-      (:strike-through t)))))
- '(pos-tip-background-color "#073642")
- '(pos-tip-foreground-color "#93a1a1")
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
- '(term-default-bg-color "#002b36")
- '(term-default-fg-color "#839496")
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
-     (40 . "#ff7f00")
-     (60 . "#ffbf00")
-     (80 . "#b58900")
-     (100 . "#ffff00")
-     (120 . "#ffff00")
-     (140 . "#ffff00")
-     (160 . "#ffff00")
-     (180 . "#859900")
-     (200 . "#aaff55")
-     (220 . "#7fff7f")
-     (240 . "#55ffaa")
-     (260 . "#2affd4")
-     (280 . "#2aa198")
-     (300 . "#00ffff")
-     (320 . "#00ffff")
-     (340 . "#00ffff")
-     (360 . "#268bd2"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (quote
-    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))))
+ '(ediff-split-window-function 'split-window-horizontally))
 
 ;; Don't screw up my window configuration after I leave ediff.
 
@@ -1222,7 +1146,7 @@ automatically updates the diff to reflect the change."
 ;;    ="this string"=, because the quotation marks causes problems.
 
 (setcar (nthcdr 2 org-emphasis-regexp-components) " \t\n,")
-
+(custom-set-variables `(org-emphasis-alist ',org-emphasis-alist))
 
 ;; TODO: Make =o= start a new header.
 
@@ -1277,19 +1201,19 @@ automatically updates the diff to reflect the change."
 (add-to-list 'org-capture-templates
              '("1" "Assignment (CIS 381)"
                entry (file+headline "~/Dropbox/org/school.org" "CIS 381")
-               "**** %?\n" ))
+               "**** TODO %?\n" ))
 (add-to-list 'org-capture-templates
              '("2" "Assignment (CIS 467)"
                entry (file+headline "~/Dropbox/org/school.org" "CIS 467")
-               "**** %?\n" ))
+               "**** TODO %?\n" ))
 (add-to-list 'org-capture-templates
              '("3" "Assignment (CIS 481)"
                entry (file+headline "~/Dropbox/org/school.org" "CIS 481")
-               "**** %?\n" ))
+               "**** TODO %?\n" ))
 (add-to-list 'org-capture-templates
              '("4" "Assignment (CIS 499)"
                entry (file+headline "~/Dropbox/org/school.org" "CIS 499")
-               "**** %?\n" ))
+               "**** TODO %?\n" ))
 
 ;; MobileOrg
 ;;    MobileOrg will let me sync my agenda to my phone, which will then sync
@@ -1366,9 +1290,3 @@ automatically updates the diff to reflect the change."
         (message "Loading init.el...done (%.3fs)" elapsed))))
 
 (add-hook 'after-init-hook 'aw/get-init-time)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
